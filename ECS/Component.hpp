3ceac2b1eeec0616626f0d65a188c158e35b89a9
Alexandre <ablonalexandre@gmail.com>
2018-01-09 16:55:34 +0100

    git ignore + start ecs

---
+++ ECS/Component.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Alexandre on 1/9/2018.
+//
+
+#ifndef RTYPE_COMPONENT_HPP
+#define RTYPE_COMPONENT_HPP
+
+class Component
+{
+
+};
+
+#endif //RTYPE_COMPONENT_HPP
--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 //
-// Created by sentrance on 09/01/18.
+// Created by Alexandre on 1/9/2018.
 //
 
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "hello world !" << std::endl;
+    return (0);
+}
