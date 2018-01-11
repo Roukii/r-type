04f2e7c4375c66ab2ac6ca304a71ade81859b580
Alexandre <ablonalexandre@gmail.com>
2018-01-11 14:17:57 +0100

    add basic udp server + basic client udp

--- server/include/ServerUdp.hpp
+++ server/include/ServerUdp.hpp
@@ -6,8 +6,16 @@
 #define R_TYPE_SERVERUDP_HPP
 
 #define PORT_SERVER 4242
+#define MAX_SIZE_MSG 2048
 
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <functional>
+#include <memory>
+#include <vector>
+#include <string>
 
 namespace RTypeServer
 {
@@ -24,9 +32,15 @@ namespace RTypeServer
 
     private:
         void startReceive();
+        void handleReceive(const boost::system::error_code& error, std::size_t);
+        void handleSend(boost::shared_ptr<std::string>, const boost::system::error_code&, std::size_t);
     private:
         //boost::asio::io_service         &_service;
         boost::asio::ip::udp::socket    _socket;
+        boost::asio::ip::udp::endpoint _remoteEndpoint;
+        boost::array<char, 1> recv_buffer_;
+        char test[MAX_SIZE_MSG];
+        //std::vector<char> _message;
     };
 }
 
--- server/src/ServerUdp.cpp
+++ server/src/ServerUdp.cpp
@@ -2,13 +2,16 @@
 // Created by Alexandre on 1/10/2018.
 //
 
-#include "../include/ServerUdp.hpp"
+#include <iostream>
+#include "ServerUdp.hpp"
 
 namespace RTypeServer
 {
     ServerUdp::ServerUdp(boost::asio::io_service &io_service)
         : _socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), PORT_SERVER))
     {
+        std::cout << _socket.local_endpoint().address().to_string() << std::endl;
+        std::cout << "start receive" << std::endl;
         startReceive();
     }
 
@@ -17,7 +20,48 @@ namespace RTypeServer
 
     void ServerUdp::startReceive()
     {
-//        _socket.async_receive(boost)
+        _socket.async_receive_from(
+                boost::asio::buffer(test, MAX_SIZE_MSG), _remoteEndpoint,
+                [this](const boost::system::error_code& error, // Result of operation.
+                       std::size_t bytes_transferred)
+                {
+                    if (!error || error == boost::asio::error::message_size)
+                    {
+                        boost::shared_ptr<std::string> message(
+                                new std::string("Wilkomen WARLD !"));
+
+                        test = std::string("test wilkomen").c_str();
+                        _socket.async_send_to(boost::asio::buffer(*message), _remoteEndpoint,
+                                              boost::bind(&ServerUdp::handleSend, this, message,
+                                                          boost::asio::placeholders::error,
+                                                          boost::asio::placeholders::bytes_transferred));
+                    }
+                    startReceive();
+
+                });
+
     }
 
+    void ServerUdp::handleReceive(const boost::system::error_code& error,
+                       std::size_t /*bytes_transferred*/)
+    {
+        if (!error || error == boost::asio::error::message_size)
+        {
+            boost::shared_ptr<std::string> message(
+                    new std::string("Wilkomen WARLD !"));
+
+            _socket.async_send_to(boost::asio::buffer(*message), _remoteEndpoint,
+                                  boost::bind(&ServerUdp::handleSend, this, message,
+                                              boost::asio::placeholders::error,
+                                              boost::asio::placeholders::bytes_transferred));
+        }
+        startReceive();
+    }
+
+    void ServerUdp::handleSend(boost::shared_ptr<std::string> /*message*/,
+                     const boost::system::error_code& /*error*/,
+                     std::size_t /*bytes_transferred*/)
+    {
+        std::cout << "send succesfull" << std::endl;
+    }
 }
