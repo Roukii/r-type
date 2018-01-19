af8d3397f07dde9e03a0edc7a77c6f1cadc9f43a
Samy Fichenick <sfichenick@gmail.com>
2018-01-19 16:35:09 +0100

    LibUGL.dll

--- client/Lib/include/ILib.hpp
+++ client/Lib/include/ILib.hpp
@@ -10,13 +10,13 @@
 class ILib {
 public:
 	virtual ~ILib() = default;
-    	virtual void		loadLib() = 0;
-    	virtual void		loadSprite(const std::string &path, const std::string &name) = 0;
+	virtual void		loadLib() = 0;
+	virtual void		loadSprite(const std::string &path, const std::string &name) = 0;
 	virtual void		deleteSprite(const std::string&) = 0;
-    	virtual void		loadFont(const std::string &path) = 0;
-    	virtual void		loadText(float x, float y, unsigned int size, const std::string &str, const std::string &name) = 0;
-    	virtual void		loadSound(const std::string &path, const std::string &name) = 0;
-    	virtual void		loadMusic(const std::string &path, const std::string &name) = 0;
+	virtual void		loadFont(const std::string &path) = 0;
+	virtual void		loadText(float x, float y, unsigned int size, const std::string &str, const std::string &name) = 0;
+	virtual void		loadSound(const std::string &path, const std::string &name) = 0;
+	virtual void		loadMusic(const std::string &path, const std::string &name) = 0;
 };
 
 #endif //R_TYPE_ILIB_HPP
